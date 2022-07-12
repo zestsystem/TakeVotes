f8f0cccd621b7c8611410701a5e00943b54bfc73
Mike Yim <mikeyim@unitetheculture.org>
2022-07-12 18:52:32 +0900

    working trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import { prisma } from '../db/client';
+
 import { trpc } from '../utils/trpc';
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(['hello']);
+  const { data, isLoading } = trpc.useQuery(['getAllQuestions']);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data.greeting}</div>;
+  return <div>{data[0]?.question}</div>;
 };
 
 export default Home;
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
-};
